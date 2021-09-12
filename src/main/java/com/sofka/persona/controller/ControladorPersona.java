27caf919e6fd394525b6f9c0f0a00b2304eaf607
DanielaVargas <daniela.03v@gmail.com>
2021-09-12 12:08:26 -0500

    Se agrega el servicio listar y validacion en postman

--- src/main/java/com/sofka/persona/controller/ControladorPersona.java
+++ src/main/java/com/sofka/persona/controller/ControladorPersona.java
@@ -1,10 +1,29 @@
 package com.sofka.persona.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sofka.persona.entity.Persona;
+import com.sofka.persona.service.InferfasServiciosPersona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("api")
-public class controladorPersona {
+public class ControladorPersona {
+
+    @Autowired
+    private InferfasServiciosPersona servicio;
+
+    @GetMapping(value = "listarPersonas")
+    public Iterable<Persona> listarPersonas() {
+        return servicio.listar();
+
+    }
+
+    @PostMapping(value = "guardarPersona")
+    public Persona guardarPersona(@RequestBody Persona persona) {
+        return servicio.guardar(persona);
+
+    }
+
 
 }
--- src/main/java/com/sofka/persona/service/ServiciosPersona.java
+++ src/main/java/com/sofka/persona/service/ServiciosPersona.java
@@ -1,2 +1,40 @@
-package com.sofka.persona.service;public class ServiciosPersona {
+package com.sofka.persona.service;
+
+import com.sofka.persona.entity.Persona;
+import com.sofka.persona.repository.InterfacesRepositorioPersona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServiciosPersona implements  InferfasServiciosPersona{
+
+    @Autowired
+    private InterfacesRepositorioPersona data;
+
+    @Override
+    public List<Persona> listar() {
+        return (List<Persona>) data.findAll();
+    }
+
+    @Override
+    public Persona ListerId(int id) {
+        return null;
+    }
+
+    @Override
+    public Persona guardar(Persona persona) {
+        return data.save(persona);
+    }
+
+    @Override
+    public void borrar(int id) {
+
+    }
+
+    @Override
+    public Persona actualizar(Persona persona) {
+        return null;
+    }
 }
